9f0bf8a945a4e29432328e71420d75c5716a1d34
Henry P B <mahe2di@gamil.com>
2023-05-12 22:42:46 -0500

    cambio_2

--- src/redux/controllers/characters.ts
+++ src/redux/controllers/characters.ts
@@ -4,11 +4,11 @@ import { setCharacters } from "../slices/slices";
 export const getCharacters = () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return (dispatch: any) => {
-    axios
-      .get(`https://rickandmortyapi.com/api/character`)
-      .then((response) => {
-        dispatch(setCharacters(response.data));
-      })
-      .catch((error) => console.log(error));
+    // axios
+    //   .get(`https://rickandmortyapi.com/api/character`)
+    //   .then((response) => {
+    //     dispatch(setCharacters(response.data));
+    //   })
+    //   .catch((error) => console.log(error));
   };
 };
